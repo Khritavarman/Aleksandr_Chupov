d4482e70570d97f3a506fdeb22b78b875b0ea8a5
Khritavarman <Khritavarman@gmail.com>
2015-11-02 16:55:45 +0300

    New changelist

--- dz_3/Cars_Ver.2/db.py
+++ dz_3/Cars_Ver.2/db.py
@@ -3,44 +3,48 @@
 # Сохраняем данные в файл 
 def pickle_save(data, path):
 
-	import pickle
-	f = open(path, 'wb')
-	pickle.dump(data,f)
-	f.close
+    import pickle
+    f = open(path, 'wb')
+    pickle.dump(data,f)
+    f.close
 
 # Загружаем данные из файла
 def pickle_load(path):
-	
-	import pickle
-	try:
-		f = open(path, 'rb')
-		data = pickle.load(f)
-	except EOFError:
-		data = {}
-	return data
+
+    import os
+
+    import pickle
+    try:
+        f = open(path, 'rb')
+        data = pickle.load(f)
+    except FileNotFoundError:
+        data = {}
+        
+    return data
 
 #Вводим данные
 def data_input(data, path):
 
 
-	while True:	
+    while True:	
 
-		car_model = input('Введите марку машины: ')
-		car_model.replace(' ','').lower()		
-		if car_model == 'x':
-			break
+        car_model = input('Введите марку машины (для выхода нажмите Х): ')
+        car_model.replace(' ','').lower()		
 
-		elif car_model.isalpha() == False:
-			print('Название машины может состоять только из букв, попробуйте еще раз: ')
-			continue		
-		car_power = input('Введите мощность двигателя: ')
-		if car_power.isnumeric() == False:
-			print('Значение мощности двигателя может состоять только из цифр, попробуйте еще раз: ')
-			continue		
+        if car_model == 'x': 
+            break
+        elif car_model.isalpha() == False:
+            print('Название машины может состоять только из букв, попробуйте еще раз: ')
+            continue
 
-		data [car_model] = car_power
+        car_power = input('Введите мощность двигателя: ')
+        if car_power.isnumeric() == False:
+            print('Значение мощности двигателя может состоять только из цифр, попробуйте еще раз: ')
+            continue
 
-		pickle_save(data, path)
+        data [car_model] = car_power
+
+        pickle_save(data, path)
 
 #--------------------------------- 
 #Вторая часть программы (добавление координат)
@@ -55,51 +59,58 @@ def new_pickle_save(new_data, new_path):
 
 # Загружаем новые данные из файла
 def new_pickle_load(new_path):
-	
-	import pickle
-	try:
-		f = open(new_path, 'rb')
-		new_data = pickle.load(f)
-	except EOFError:
-		data = {}
-	return new_data
+
+    import os
+    import pickle
+
+    try:
+        f = open(new_path, 'rb')
+        new_data = pickle.load(f)
+    except FileNotFoundError:
+        data = {}
+    else:
+    	print('Вы вошли в новую базу данных')
+    finally:
+    	f.close
+    return data
 
 # Изменение словаря, добавление значений координат и пробега
 def create_new_data(data, path, new_path, new_data):
 
-	new_data = new_pickle_load(new_path)
+    new_data = new_pickle_load(new_path)
 
-	for i in data:
-		if i.replace(' ','').isalpha() == True:
-			print('\n', i)
-			p = input('\nДля того, чтобы редактировать данные об автомобиле, нажмите 1\nЧтобы пропустить редактирование, нажмите любую клавишу\n')
+    for i in data:
+        if i.replace(' ','').isalpha() == True:
+            print('\n', i)
 
-			if p == '1':
-				number = input('Введите номер: ')
-				coorX = float(input('введите координату X: '))
-				coorY = float(input('введите координату Y: '))
-				dist = float(input('Введите величину пробега: '))
-				new_data [number] = [i, coorX, coorY, dist]
-			else: continue
+            p = input('\nДля того, чтобы редактировать данные об автомобиле, нажмите 1\nЧтобы пропустить редактирование, нажмите любую клавишу\n')
 
-	new_pickle_save(new_data, new_path)
+            if p == '1':
+                number = input('Введите номер: ')
+                coorX = float(input('введите координату X: '))
+                coorY = float(input('введите координату Y: '))
+                dist = float(input('Введите величину пробега: '))
+                new_data [number] = [i, coorX, coorY, dist]
+            else: continue
+
+    new_pickle_save(new_data, new_path)
 
 # Перемещение в другие координаты
 def car_move(new_data, new_path):
 
-	import math
-	new_data = new_pickle_load(new_path)
-	
-	car_to_edit = input('Введите номер автомобиля, который хотите отредактировать: \n')
-	
-	for i in new_data:
-		if car_to_edit == i:
-			coorX = float(input ('Введите новую координату Х: '))
-			coorY = float(input ('Введите новую координату У: '))
-			delta1 = abs(coorX - float(new_data[i][1]))
-			delta2 = abs(coorY - float(new_data[i][2]))
-			new_data[i][1] = coorX
-			new_data[i][2] = coorY
-			new_data[i][3] = float(new_data[i][3]) + round(math.sqrt(delta1 ** 2 + delta2 ** 2), 2)
-	
-	new_pickle_save(new_data, new_path)
+    import math
+    new_data = new_pickle_load(new_path)
+
+    car_to_edit = input('Введите номер автомобиля, который хотите отредактировать: \n')
+
+    for i in new_data:
+        if car_to_edit == i:
+            coorX = float(input ('Введите новую координату Х: '))
+            coorY = float(input ('Введите новую координату У: '))
+            delta1 = abs(coorX - float(new_data[i][1]))
+            delta2 = abs(coorY - float(new_data[i][2]))
+            new_data[i][1] = coorX
+            new_data[i][2] = coorY
+            new_data[i][3] = float(new_data[i][3]) + round(math.sqrt(delta1 ** 2 + delta2 ** 2), 2)
+
+    new_pickle_save(new_data, new_path)
--- dz_3/Cars_Ver.2/debug.py
+++ dz_3/Cars_Ver.2/debug.py
@@ -4,18 +4,18 @@ import db
 
 def func_output(data, path):
 
-	data = db.pickle_load(path)
+    data = db.pickle_load(path)
 
-	cars = list(data)
-	cars.sort()
+    cars = list(data)
+    cars.sort()
 
-	for i in cars:
-		a = print(i, ' - ' , data[i], end = ', ')
-	return data
+    for i in cars:
+        a = print(i, ' - ' , data[i], end = ', ')
+    return data
 
-#Функция вывода на экран результатов работы первой части программы
+#Функция вывода на экран результатов работы второй части программы
 def new_func_output(new_data, new_path):
 
-	new_data = db.new_pickle_load(new_path)
+    new_data = db.new_pickle_load(new_path)
 
-	print(new_data)
+    print(new_data)
